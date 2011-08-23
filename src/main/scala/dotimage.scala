5a601043c267470af82d03185f4405bf969f6dee
Michael Schmitz <michael@schmitztech.com>
2011-08-22 23:28:10 -0700

    rm

--- src/main/scala/dotimage.scala
+++
@@ -1,6 +0,0 @@
-import javax.servlet.http._
-
-class DotImage extends HttpServlet {
-  override def doGet(req: HttpServletRequest, resp: HttpServletResponse) =
-    resp.getWriter().print("Hello World!")
-}
